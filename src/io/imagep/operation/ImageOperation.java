cef331a38be62fa3c24a663470385fb1e69ef83c
hirohito-protagonist <hirohito.protagonist@protonmail.com>
2018-05-07 19:57:16 +0200

    refactor: reduce code duplication
    Introduce internal function to operate on transformation
    image pixels.

--- src/io/imagep/operation/ImageOperation.java
+++ src/io/imagep/operation/ImageOperation.java
@@ -4,33 +4,23 @@ import com.sun.istack.internal.NotNull;
 import javafx.scene.image.*;
 
 import java.nio.IntBuffer;
-import java.util.Arrays;
-import java.util.stream.Stream;
+import java.util.function.Function;
 
 
 public class ImageOperation {
 
     public static WritableImage grayScale(@NotNull final Image originalImage) {
 
-        int width = (int)originalImage.getWidth();
-        int height = (int)originalImage.getHeight();
-
-        WritableImage image = new WritableImage(width, height);
-
-        PixelWriter pixelWriter = image.getPixelWriter();
-        PixelReader pixelReader = originalImage.getPixelReader();
-
-        int[] buffer = new int[width * height];
-        WritablePixelFormat<IntBuffer> pixelFormat = PixelFormat.getIntArgbInstance();
-        pixelReader.getPixels(0, 0, width, height, pixelFormat, buffer, 0, width);
-        ImageOperation.grayScalePixels(buffer);
-        pixelWriter.setPixels(0, 0, width, height, pixelFormat, buffer, 0, width);
-
-        return image;
+        return ImageOperation.internalImageOperation(originalImage, ImageOperation::grayScalePixels);
     }
 
     public static WritableImage invert(@NotNull final Image originalImage) {
 
+        return internalImageOperation(originalImage, ImageOperation::invertPixels);
+    }
+
+    private static WritableImage internalImageOperation(@NotNull final Image originalImage, Function<int[], int[]> pixelsTransformation) {
+
         int width = (int)originalImage.getWidth();
         int height = (int)originalImage.getHeight();
 
@@ -42,13 +32,14 @@ public class ImageOperation {
         int[] buffer = new int[width * height];
         WritablePixelFormat<IntBuffer> pixelFormat = PixelFormat.getIntArgbInstance();
         pixelReader.getPixels(0, 0, width, height, pixelFormat, buffer, 0, width);
-        ImageOperation.invertPixels(buffer);
+        buffer = pixelsTransformation.apply(buffer);
         pixelWriter.setPixels(0, 0, width, height, pixelFormat, buffer, 0, width);
 
         return image;
     }
 
-    private static void invertPixels(@NotNull int[] pixels) {
+    /** Creates side effect on pixels collection */
+    private static int[] invertPixels(@NotNull int[] pixels) {
 
         for (int i = 0; i < pixels.length; i++) {
             int argb = pixels[i];
@@ -58,9 +49,11 @@ public class ImageOperation {
             int b = argb & 0xff;
             pixels[i] = (a << 24) | ((255 - r) << 16) | ((255 - g) << 8) | (255 - b);
         }
+        return pixels;
     }
 
-    private static void grayScalePixels(@NotNull int[] pixels) {
+    /** Creates side effect on pixels collection */
+    private static int[] grayScalePixels(@NotNull int[] pixels) {
 
         for (int i = 0; i < pixels.length; i++) {
             int argb = pixels[i];
@@ -71,5 +64,6 @@ public class ImageOperation {
             int color = (int) (0.299 * r + 0.587 * g + 0.114 * b);
             pixels[i] = (a << 24) | (color << 16) | (color << 8) | color;
         }
+        return pixels;
     }
 }
